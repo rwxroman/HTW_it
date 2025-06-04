75b2e1da7de806fa6c95cf4a48c4ca22768b0bef
rwxroman <135510863+rwxroman@users.noreply.github.com>
2025-06-04 11:16:07 +0200

    Disabled the service-worker (dont need yet)

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,21 +1,19 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('calendar-pwa').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './styles.css',
-        './app.js',
-        './manifest.json',
-        './icons/icon-192.png',
-        './icons/icon-512.png'
-      ]);
-    })
-  );
-});
+// self.addEventListener('install', (e) => {
+//   e.waitUntil(
+//     caches.open('calendar-pwa').then((cache) => {
+//       return cache.addAll([
+//         './',
+//         './index.html',
+//         './styles.css',
+//         './app.js',
+//         './manifest.json'
+//       ]);
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
+// self.addEventListener('fetch', (e) => {
+//   e.respondWith(
+//     caches.match(e.request).then((response) => response || fetch(e.request))
+//   );
+// });
